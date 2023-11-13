0ee391d94725172f34ecf41b8f3b10ba03f6d688
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-13 19:01:35 +0000

    initial folders

---
+++ src/components/ArtworkDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArtworkDisplay = () => {
+  return (
+    <div>ArtworkDisplay</div>
+  )
+}
+
+export default ArtworkDisplay
---
+++ src/components/Artworks/Artowrk.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Artowrk = () => {
+  return (
+    <div>Artowrk</div>
+  )
+}
+
+export default Artowrk
