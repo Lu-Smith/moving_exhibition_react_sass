860f365913e88080b81a49b34f5e4b78de9f3967
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-13 20:03:36 +0000

    Updated Navigation added Scroll

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavSvg from './NavSvg'
+import Scroll from './Scroll'
 
 const Navigation = () => {
+
+  const [scroll, setScroll] = useState(false);
+
+  const handleScroll = () => {
+    setScroll(!scroll);
+  }
+
   return (
     <div>
         <h2>Scorll to explore</h2>
         <div className="scrollButton">
-            <NavSvg />
+          <div onClick={handleScroll}><NavSvg /></div>
+          {scroll && <Scroll />}
         </div>
     </div>
   )
