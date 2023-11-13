a612c10111e2bf88c7bd61d07c98153e841a694e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-13 19:57:56 +0000

    Updated Navigation.tsx

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavSvg from './NavSvg'
+
+const Navigation = () => {
+  return (
+    <div>
+        <h2>Scorll to explore</h2>
+        <div className="scrollButton">
+            <NavSvg />
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
