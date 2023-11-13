aeefcc9e52bf200cd6db39e966b4cf5ae0a19d5d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-13 20:32:26 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import MainPage from './components/Artworks/MainPage';
 import Navigation from './components/Navigation/Navigation';
 import PageOne from './components/Artworks/PageOne';
+import PageTwo from './components/Artworks/PageTwo';
+import PageThree from './components/Artworks/PageThree';
+import PageFour from './components/Artworks/PageFour';
+import PageFive from './components/Artworks/PageFive';
 
 function App() {
 
@@ -12,12 +16,13 @@ function App() {
     setScroll(!scroll);
   }
 
+  const pages = [MainPage, PageOne, PageTwo, PageThree, PageFour, PageFive]
 
   return (
     <div className="App">
-      <div>
-        <MainPage />
-        <PageOne />
+      <div className='inViewPage'>
+      </div>
+      <div className='nextPage'>
       </div>
       <Navigation scroll={scroll} handleScroll={handleScroll} />
     </div>
--- src/components/ArtworkDisplay.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArtworkDisplay = () => {
-  return (
-    <div>ArtworkDisplay</div>
-  )
-}
-
-export default ArtworkDisplay
