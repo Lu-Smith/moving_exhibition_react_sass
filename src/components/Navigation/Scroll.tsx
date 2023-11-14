639757a25a8d6974beecd017074366baef1d5d8d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-14 15:05:42 +0000

    Updated Scroll.tsx

--- src/components/Navigation/Scroll.tsx
+++ src/components/Navigation/Scroll.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Scroll = () => {
+
+  const years = [2018, 2019, 2020, 2021, 2022, 2023]
   return (
-    <div>Scroll</div>
+    <div className='Scroll'>
+      {years.map((year, index) => {
+        return <div key={index}>
+          {year}
+        </div>
+      })}
+
+    </div>
   )
 }
 
