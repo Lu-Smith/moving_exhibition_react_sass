418364fc977f0047a6873ddec2dff1cd42fa1b9c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-14 16:40:33 +0000

    Updated Scroll.tsx

--- src/components/Navigation/Scroll.tsx
+++ src/components/Navigation/Scroll.tsx
@@ -2,12 +2,43 @@ import React from 'react'
 
 const Scroll = () => {
 
-  const years = [2018, 2019, 2020, 2021, 2022, 2023]
+  const years = [
+    {
+      year: "2018",
+      section: ["", "", "", ""]
+    }, 
+    {
+      year: "2019",
+      section: ["", "", "", ""]
+    },
+    {
+      year: "2020",
+      section: ["", "", "", ""]
+    },
+    {
+      year: "2021",
+      section: ["", "", "", ""]
+    },
+    {
+      year: "2022",
+      section: ["", "", "", ""]
+    },
+    {
+      year: "2023",
+      section: ["", "", "", ""]
+    }
+  ]
   return (
     <div className='Scroll'>
       {years.map((year, index) => {
         return <div key={index}>
-          {year}
+          <div>{year.year}</div>
+          {year.section.map((section, index) => {
+            return <div key={index} className='yearSection'>
+              {section}
+            </div>
+          })}
+          
         </div>
       })}
 
