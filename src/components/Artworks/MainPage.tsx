6fb60327a96c2507c3bbfa4538c432d6db624410
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-14 15:01:50 +0000

    Updated MainPage

--- src/components/Artworks/MainPage.tsx
+++ src/components/Artworks/MainPage.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const MainPage = () => {
   return (
     <div>
-        <h1>Art Through Time</h1>
+      <h1>Art Through Time</h1>
+      <h2>Throughout the years, both the realm of art and the broader world have undergone transformative changes. From the inception of my career as an artist, I have witnessed a continual evolution in my artwork, a reflection of the dynamic interplay between my creative journey and the shifting landscapes of our shared existence.</h2>
     </div>
   )
 }
