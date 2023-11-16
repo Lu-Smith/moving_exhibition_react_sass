1659f87a5748f1472bb827aa61327342b20ed885
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 12:14:48 +0000

    Updated PageOne
    Starting adding atworks

--- src/components/Artworks/PageOne.tsx
+++ src/components/Artworks/PageOne.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import InteractiveBackground from './InteractiveBackground'
 import TopBackground from './TopBackground'
+import ZinShadows from '../../assets/paintings/Z in Shadows by Luna Smith.jpg'
 
 const PageOne = () => {
   return (
@@ -11,6 +12,11 @@ const PageOne = () => {
         </div>
         <div className="backBackground">
           <InteractiveBackground />
+        </div>
+        <div className="paintings">
+          <div className="imageContainerLandscape">
+            <img src={ZinShadows} alt="Z in Shadows, oil painting by Luna Smith" />
+          </div>
         </div>
       </div>
     </div>
