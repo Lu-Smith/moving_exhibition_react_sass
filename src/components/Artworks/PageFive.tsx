c5254e98aa33f394f01147af8b1394b38cbe304d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 12:50:34 +0000

    Updated PageFour

--- src/components/Artworks/PageFive.tsx
+++ src/components/Artworks/PageFive.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import TopBackground from './TopBackground'
 import InteractiveBackground from './InteractiveBackground'
+import Reflections from '../../assets/paintings/Autumn Reflection by Luna Smith.jpg'
+import WhiteSnake from '../../assets/paintings/Hatching White Snake by Luna Smith.jpg'
+import RainbowEgg from '../../assets/paintings/Rainbow-Egg-by-Luna-Smith.jpg'
 
 const PageFive = () => {
   return (
@@ -12,6 +15,17 @@ const PageFive = () => {
       <div className="backBackground">
         <InteractiveBackground />
       </div>
+      <div className="paintings">
+          <div className="imageContainerLandscape one">
+            <img src={Reflections} alt="Shrimp, oil painting by Luna Smith" />
+          </div>
+          <div className="imageContainerPortrait two">
+            <img src={WhiteSnake} alt="Dream, oil painting by Luna Smith" />
+          </div>
+          <div className="imageContainerPortrait three">
+            <img src={RainbowEgg} alt="Owl, oil painting by Luna Smith" />
+          </div>
+        </div>
     </div>
   </div>
   )
