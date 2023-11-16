f7d2d8eb0530b7a5113166540a3894fdf65b28a5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 16:49:54 +0000

    Updated AnimationPaths.tsx

--- src/components/Artworks/AnimationPaths.tsx
+++ src/components/Artworks/AnimationPaths.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { heart, art, book } from '../../assets/animationPaths';
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { getIndex, useFlubber } from '../../use-flubber';
+
+const paths = [art, heart, book];
+const colors = [
+  "#00cc88",
+  "#0099ff",
+  "#8855ff",
+];
 
 const AnimationPaths = () => {
+    const [pathIndex, setPathIndex] = useState(0);
+    const progress = useMotionValue(pathIndex);
+    const fill = useTransform(progress, paths.map(getIndex), colors);
+    const path = useFlubber(progress, paths);
+
+    React.useEffect(() => {
+        const animation = animate(progress, pathIndex, {
+          duration: 0.8,
+          ease: "easeInOut",
+          onComplete: () => {
+            if (pathIndex === paths.length - 1) {
+              progress.set(0);
+              setPathIndex(1);
+            } else {
+              setPathIndex(pathIndex + 1);
+            }
+          }
+        });
+    
+        return () => animation.stop();
+      }, [pathIndex]);
+
   return (
-    <div>AnimationPaths</div>
+        <svg width="400" height="400">
+        <g transform="translate(10 10) scale(17 17)">
+            <motion.path fill={fill} d={path} />
+        </g>
+        </svg>
   )
 }
 
