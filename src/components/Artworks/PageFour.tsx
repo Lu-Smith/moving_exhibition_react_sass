5db0e8394f03e7b94476aa40c5bed5b6610fb87c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 12:46:10 +0000

    Updated PageFour

--- src/components/Artworks/PageFour.tsx
+++ src/components/Artworks/PageFour.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import TopBackground from './TopBackground'
+import WinterSummer from '../../assets/paintings/LunaSmith_WhenWinterMeetsSummer.jpg'
+import WinterBreeze from '../../assets/paintings/LunaSmith_Winter Breeze.jpg'
+import ItalianAlps from '../../assets/paintings/Italian-Alps-by-Luna-Smith.jpg'
 
 const PageFour = () => {
   return (
-    <div className="frontBackground">
-    <TopBackground />
+    <div className='Page'>
+    <div className="backgroundContainer">
+      <div className="frontBackground">
+        <TopBackground />
+      </div>
+      <div className="paintings">
+        <div className="imageContainerLandscape one">
+          <img src={WinterSummer} alt="When Winter Meets Summer, oil painting by Luna Smith" />
+        </div>
+        <div className="imageContainerLandscape two">
+          <img src={WinterBreeze} alt="Winter Breeze, oil painting by Luna Smith" />
+        </div>
+        <div className="imageContainerPortrait three odd">
+          <img src={ItalianAlps} alt="Italian Alps, oil painting by Luna Smith" />
+        </div>
+      </div>
+    </div>
   </div>
   )
 }
