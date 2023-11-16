20e84814fb0d2786529905351336ddd7857ca47e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 12:34:31 +0000

    Updated PageTwo

--- src/components/Artworks/PageTwo.tsx
+++ src/components/Artworks/PageTwo.tsx
@@ -2,22 +2,25 @@ import React from 'react'
 import TopBackground from './TopBackground'
 import GlenEtive from '../../assets/paintings/The-Song-of-the-Glen-Etive-by-Luna-Smith.jpg'
 import TrappedWind from '../../assets/paintings/The-Trapped-Wind.jpg'
+import SecretGarden from '../../assets/paintings/Secret Garden by Luna Smith.jpg'
 
 const PageTwo = () => {
   return (
-    <div>
-        <div className="frontBackground">
-          <TopBackground />
-        </div>
-        <div className="paintings">
-          <div className="imageContainerLandscape one">
-            <img src={GlenEtive} alt="The song of Glen Etive, oil painting by Luna Smith" />
+    <div className='Page'>
+        <div className="backgroundContainer">
+          <div className="frontBackground">
+            <TopBackground />
           </div>
-          <div className="imageContainerLandscape two">
-            <img src={TrappedWind} alt="The Trapped Wind, oil painting by Luna Smith" />
-          </div>
-          <div className="imageContainerPortrait three">
-            <img src={WomenNature} alt="Women Nature, oil painting by Luna Smith" />
+          <div className="paintings">
+            <div className="imageContainerLandscape one">
+              <img src={GlenEtive} alt="The song of Glen Etive, oil painting by Luna Smith" />
+            </div>
+            <div className="imageContainerLandscape two">
+              <img src={TrappedWind} alt="The Trapped Wind, oil painting by Luna Smith" />
+            </div>
+            <div className="imageContainerPortrait three">
+              <img src={SecretGarden} alt="SecretGarden, oil painting by Luna Smith" />
+            </div>
           </div>
         </div>
     </div>
