c495f8d2ebc830dc1918b8450703bf49b67ca0ef
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 16:26:22 +0000

    Added animation to PageSix

--- src/components/Artworks/PageSix.tsx
+++ src/components/Artworks/PageSix.tsx
@@ -4,6 +4,7 @@ import Diabaig from '../../assets/paintings/The-Whishper-of-the-Diabaig--by-Luna
 import GoldenTrees from '../../assets/paintings/Shimmering Golden Trees.jpg'
 import DreamsFuture from '../../assets/paintings/Dreams of a Future Self by Luna Smith.jpg'
 import Universe from '../../assets/paintings/The Reflection of my Universe by Luna Smith.jpg'
+import { motion } from 'framer-motion'
 
 const PageSix: React.FC = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -27,18 +28,35 @@ const PageSix: React.FC = () => {
         <TopBackground />
       </div>
       <div className="paintings">
-          <div className="imageContainerLandscape one odd" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+          <div className="imageContainerLandscape one odd" 
+          style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
             <img src={Diabaig} alt="The Wishper of Diabaig, oil painting by Luna Smith" />
           </div>
-          <div className="imageContainerLandscape two odd" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+          <div className="imageContainerLandscape two odd" 
+          style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
             <img src={GoldenTrees} alt="The Golden Trees, oil painting by Luna Smith" />
           </div>
-          <div className="imageContainerPortrait three" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+          <div className="imageContainerPortrait three" 
+          style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
             <img src={DreamsFuture} alt="Dreams of Future Self, oil painting by Luna Smith" />
           </div>
-          <div className="imageContainerPortrait four" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+          <motion.div 
+          className="imageContainerPortrait four" 
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 2,
+            ease: [0, 0.71, 0.2, 1.01],
+            scale: {
+              type: "spring",
+              damping: 5,
+              stiffness: 100,
+              restDelta: 0.001
+            }
+          }}
+          style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
             <img src={Universe} alt="the Reflection of my Universe, oil painting by Luna Smith" />
-          </div>
+          </motion.div>
       </div>
     </div>
   </div>
