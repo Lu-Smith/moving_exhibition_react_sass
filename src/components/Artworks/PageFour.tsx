eeef7f0d70b2ab2f28ccf704f78870cfe6462252
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 16:26:01 +0000

    Updated PageFour

--- src/components/Artworks/PageFour.tsx
+++ src/components/Artworks/PageFour.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import TopBackground from './TopBackground'
 import WinterSummer from '../../assets/paintings/LunaSmith_WhenWinterMeetsSummer.jpg'
 import WinterBreeze from '../../assets/paintings/LunaSmith_Winter Breeze.jpg'
 import ItalianAlps from '../../assets/paintings/Italian-Alps-by-Luna-Smith.jpg'
 import Autumn from '../../assets/paintings/Autumn-by-Luna-Smith.jpg'
 import InteractiveBackgroundSmall from './InteractiveBackgroundSmall'
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 
 const PageFour: React.FC = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -32,7 +32,8 @@ const PageFour: React.FC = () => {
           <InteractiveBackgroundSmall />
       </div>
       <div className="paintings">
-        <div className="imageContainerLandscape one" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+        <div className="imageContainerLandscape one" 
+        style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
           <img src={WinterSummer} alt="When Winter Meets Summer, oil painting by Luna Smith" />
         </div>
         <motion.div 
@@ -52,10 +53,12 @@ const PageFour: React.FC = () => {
         style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
           <img src={WinterBreeze} alt="Winter Breeze, oil painting by Luna Smith" />
         </motion.div>
-        <div className="imageContainerPortrait three odd" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+        <div className="imageContainerPortrait three odd" 
+        style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
           <img src={ItalianAlps} alt="Italian Alps, oil painting by Luna Smith" />
         </div>
-        <div className="imageContainerPortrait four" style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
+        <div className="imageContainerPortrait four" 
+        style={{ border: `4px solid rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1)` }}>
             <img src={Autumn} alt="the Reflection of my Universe, oil painting by Luna Smith" />
         </div>
       </div>
