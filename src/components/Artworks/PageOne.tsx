26d8b8d753b361f496725ad7f2ded0f858bb0a3b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 14:19:49 +0000

    Added interactive frames to PageOne and PageTwo

--- src/components/Artworks/PageOne.tsx
+++ src/components/Artworks/PageOne.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 import InteractiveBackground from './InteractiveBackground'
 import InteractiveBackgroundSmall from './InteractiveBackgroundSmall'
 import TopBackground from './TopBackground'
@@ -6,7 +7,21 @@ import ZinShadows from '../../assets/paintings/Z in Shadows by Luna Smith.jpg'
 import ScottishDream from '../../assets/paintings/Scottish Dream by Luna Smith.jpg'
 import WomenNature from '../../assets/paintings/LunaSmith_LifeArchitect.jpg'
 
-const PageOne = () => {
+const PageOne: React.FC = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: any) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  
   return (
     <div className='Page'>
       <div className="backgroundContainer">
@@ -20,13 +35,13 @@ const PageOne = () => {
           <InteractiveBackgroundSmall />
         </div>
         <div className="paintings">
-          <div className="imageContainerLandscape one">
+        <div className="imageContainerLandscape one" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
             <img src={ZinShadows} alt="Z in Shadows, oil painting by Luna Smith" />
           </div>
-          <div className="imageContainerLandscape two">
+          <div className="imageContainerLandscape two" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
             <img src={ScottishDream} alt="L in Shadows, oil painting by Luna Smith" />
           </div>
-          <div className="imageContainerPortrait three">
+          <div className="imageContainerPortrait three" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
             <img src={WomenNature} alt="Women Nature, oil painting by Luna Smith" />
           </div>
         </div>
--- src/components/Artworks/PageTwo.tsx
+++ src/components/Artworks/PageTwo.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import TopBackground from './TopBackground'
 import GlenEtive from '../../assets/paintings/The-Song-of-the-Glen-Etive-by-Luna-Smith.jpg'
 import TrappedWind from '../../assets/paintings/The-Trapped-Wind.jpg'
 import SecretGarden from '../../assets/paintings/Secret Garden by Luna Smith.jpg'
 import PineCone from '../../assets/paintings/pinecone.jpg'
 
-const PageTwo = () => {
+const PageTwo: React.FC = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: any) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className='Page'>
         <div className="backgroundContainer">
@@ -13,13 +27,13 @@ const PageTwo = () => {
             <TopBackground />
           </div>
           <div className="paintings">
-            <div className="imageContainerLandscape one">
+            <div className="imageContainerLandscape one" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
               <img src={GlenEtive} alt="The song of Glen Etive, oil painting by Luna Smith" />
             </div>
-            <div className="imageContainerLandscape two">
+            <div className="imageContainerLandscape two" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
               <img src={TrappedWind} alt="The Trapped Wind, oil painting by Luna Smith" />
             </div>
-            <div className="imageContainerPortrait three">
+            <div className="imageContainerPortrait three" style={{ border: `8px solid rgba(${mousePosition.x / window.innerWidth * 255}, ${mousePosition.y / window.innerHeight * 255}, 0, 0.5)` }}>
               <img src={SecretGarden} alt="SecretGarden, oil painting by Luna Smith" />
             </div>
             <div className="imageContainerPortrait five">
