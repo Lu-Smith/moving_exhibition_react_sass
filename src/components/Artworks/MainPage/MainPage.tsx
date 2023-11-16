8501810eee9897b1fd20f17c76440bc62be2a96a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 16:54:24 +0000

    Updated MainPage

--- src/components/Artworks/MainPage/MainPage.tsx
+++ src/components/Artworks/MainPage/MainPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import MainAnimation from './MainAnimation'
+import AnimationPaths from '../AnimationPaths'
 
 const MainPage = () => {
   return (
     <div className='MainPage'>
       <h1>Art Through Time</h1>
       <h2>Throughout the years, both the realm of art and the broader world have undergone transformative changes. From the inception of my career as an artist, I have witnessed a continual evolution in my artwork, a reflection of the dynamic interplay between my creative journey and the shifting landscapes of our shared existence.</h2>
+      <div className="pathAnimationContainer">
+        <AnimationPaths />
+      </div>
       <div className="animationContainer">
         <MainAnimation />
       </div>
