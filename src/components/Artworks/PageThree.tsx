6e0554486a82fb3db66292ca3f898e53a9455028
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 12:39:49 +0000

    Updated Page Three

--- src/components/Artworks/PageThree.tsx
+++ src/components/Artworks/PageThree.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import TopBackground from './TopBackground'
 import InteractiveBackground from './InteractiveBackground'
+import Shrimp from '../../assets/paintings/Shrimp-by-Luna-Smith.jpg'
+import Dream from '../../assets/paintings/Dream by Luna Smith.jpg'
+import Owl from '../../assets/paintings/Owl by Luna Smith.jpg'
 
 const PageThree = () => {
   return (
@@ -12,6 +15,17 @@ const PageThree = () => {
       <div className="backBackground">
         <InteractiveBackground />
       </div>
+      <div className="paintings">
+          <div className="imageContainerAnimal one">
+            <img src={Shrimp} alt="Shrimp, oil painting by Luna Smith" />
+          </div>
+          <div className="imageContainerAnimal two">
+            <img src={Dream} alt="Dream, oil painting by Luna Smith" />
+          </div>
+          <div className="imageContainerPortrait three">
+            <img src={Owl} alt="Owl, oil painting by Luna Smith" />
+          </div>
+        </div>
     </div>
   </div>
   )
