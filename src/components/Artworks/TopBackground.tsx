c119fa80a7624ca77fbb25419a9f1488a9126ca6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-16 11:23:28 +0000

    Created TopBackground

---
+++ src/components/Artworks/TopBackground.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopBackground = () => {
+  return (
+    <div className='TopBackground'>
+        <svg className="interactive-background" xmlns="http://www.w3.org/2000/svg" viewBox="10 -20 80 100">
+        <path d="M9,31 C10,10 40,10 50,30 C60,50 90,50 91,28" fill="none" stroke="#7ed0ec" stroke-width="0.2"/>
+        </svg>
+    </div>
+  )
+}
+
+export default TopBackground
