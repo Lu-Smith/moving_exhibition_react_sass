3307f410232c71ea011e8d8b248b3a4bd32af3ee
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-17 12:27:21 +0000

    Updated background in MainPage

--- src/components/Artworks/MainPage/MainPage.tsx
+++ src/components/Artworks/MainPage/MainPage.tsx
@@ -19,7 +19,7 @@ const MainPage:React.FC = () => {
   return (
     <div 
     className='MainPage'
-    style={{ background: `linear-gradient(68.3deg, rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1) 5.6%, transparent 22.7%)` }}>
+    style={{ background: `linear-gradient(90deg, transparent 4%, rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1) 4.6%, rgb(251, 248, 248) 5%, transparent 7%, rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1) 8%, rgb(251, 248, 248) 8.5%, transparent 10%, rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1) 10.6%, rgb(251, 248, 248) 11%, transparent 12%, rgba(${mousePosition.x / window.innerWidth * 55}, ${mousePosition.y / window.innerHeight * 55}, 0, 1) 13%, rgb(251, 248, 248) 14%)` }}>
       <h1>Art Through Time</h1>
       <h2>Throughout the years, both the realm of art and the broader world have undergone transformative changes. From the inception of my career as an artist, I have witnessed a continual evolution in my artwork, a reflection of the dynamic interplay between my creative journey and the shifting landscapes of our shared existence.</h2>
       <div className="animationContainer">
